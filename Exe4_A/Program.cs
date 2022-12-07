1ab01521f6ea8989a812b4bc33bb5f114bb23066
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-12-07 15:51:37 +0700

    add function if stack not empty

--- Exe4_A/Program.cs
+++ Exe4_A/Program.cs
@@ -52,6 +52,13 @@ namespace Exe4_A
                 Console.WriteLine("Stack is Empty");
                 return ;
             }
+            else
+            {
+                for(int i = 0; i < top; i++)
+                {
+                    Console.WriteLine("Item ["+ (i +1) + "]: "+ st[i]);
+                }
+            }
         }
     }
 }
